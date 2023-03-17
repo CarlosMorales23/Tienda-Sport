9f31e2a27effc1701c6bcefea68f455277400fad
Carlos Morales <carlosmora230190@gmail.com>
2023-03-17 04:37:35 -0300

    Enrutando

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 // import ItemCount from "./components/ItemCount/ItemCount";
-import ConsumiendoApis from "./components/ConsumiendoApis/ConsumiendoApis";
-import FooterPres from "./components/Footer/FooterPres";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cart from "./components/Cart/Cart";
+// import ConsumiendoApis from "./components/ConsumiendoApis/ConsumiendoApis";
+// import FooterPres from "./components/Footer/FooterPres";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import Loguin from "./components/Loguin/Loguin";
 import NavbarPres from "./components/Navbar/NavbarPres/NavbarPres";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
     return (
         <div className="App">
-            <NavbarPres />
-            <ItemListContainer saludo="Pepito Romero" />
-            <ConsumiendoApis/>
-            <FooterPres/>
-            {/* <ItemCount stock={5} initial= {1} onAdd={onAdd}/> */}
+            <BrowserRouter>
+                <NavbarPres/>
+                <Routes>
+                    <Route path="/" element={<ItemListContainer/>} />
+                    <Route path="/cart" element={<Cart/>}/>
+                    <Route path="/loguin" element={<Loguin/>}/>
+
+                    <Route path="*" element={<NotFound/>}/>
+
+                </Routes>
+            </BrowserRouter>
         </div>
-    )
+    );
 }
 
 export default App;
 
-    // const onAdd = (cantidad) =>{
-    //     alert(`Se agrego al carrito ${cantidad} elementos`);
-    // }
+// const onAdd = (cantidad) =>{
+//     alert(`Se agrego al carrito ${cantidad} elementos`);
+// }
+
+// <NavbarPres />
+// <ItemListContainer saludo="Pepito Romero" />
+// <ConsumiendoApis/>
+// <FooterPres/>
--- src/components/Navbar/Logo/Logo.jsx
+++ src/components/Navbar/Logo/Logo.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import styles from "./Logo.module.css"
 
+import { Link } from "react-router-dom";
+
 const Logo = () => {
     return (
-        <img className={styles.logo}
-            src="https://res.cloudinary.com/dybsirwia/image/upload/v1677811224/E-commerce/i_lr7dus.png" height={100}
-            alt="Logo"
-        />
+        <Link to="/">
+            <img
+                className={styles.logo}
+                src="https://res.cloudinary.com/dybsirwia/image/upload/v1677811224/E-commerce/i_lr7dus.png"
+                height={100}
+                alt="Logo"
+            />
+        </Link>
     );
 };
 
