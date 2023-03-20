c68a2448755b2f2f225f256f634b59a80723392a
Carlos Morales <carlosmora230190@gmail.com>
2023-03-19 21:43:10 -0300

    Coregido error import de cart.jsx

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Cart from "./components/Cart/Cart";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
-// import ConsumiendoApis from "./components/ConsumiendoApis/ConsumiendoApis";
-// import FooterPres from "./components/Footer/FooterPres";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import Loguin from "./components/Loguin/Loguin";
 import NavbarPres from "./components/Navbar/NavbarPres/NavbarPres";
@@ -11,28 +9,27 @@ import NotFound from "./components/NotFound/NotFound";
 
 function App() {
     return (
-        <BrowserRouter>
-            <NavbarPres />
-            <Routes>
-                <Route path="/" element={<ItemListContainer />} />
+        
+            <BrowserRouter>
+                <NavbarPres />
+                <Routes>
+                    <Route path="/" element={<ItemListContainer />} />
 
-                <Route
-                    path="/category/:categoryName"
-                    element={<ItemListContainer />}
-                />
+                    <Route path="/category/:categoryName" element={<ItemListContainer />} />
 
-                <Route path="*" element={<NotFound />} />
+                    <Route path="*" element={<NotFound />} />
 
-                <Route path="/cart" element={<Cart />} />
+                    <Route path="/cart" element={<Cart />} />
 
-                <Route path="/loguin" element={<Loguin />} />
+                    <Route path="/loguin" element={<Loguin />} />
 
-                <Route
-                    path="/ItemDetail/:id"
-                    element={<ItemDetailContainer />}
-                />
-            </Routes>
-        </BrowserRouter>
+                    <Route
+                        path="/ItemDetail/:id"
+                        element={<ItemDetailContainer />}
+                    />
+                </Routes>
+            </BrowserRouter>
+    
     );
 }
 
