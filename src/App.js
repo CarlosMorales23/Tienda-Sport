cf6206f4d21a47a86658627bfdae0a847dc8d8f8
Carlos Morales <carlosmora230190@gmail.com>
2023-03-22 22:32:13 -0300

    Creado script de Backend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Cart from "./components/Cart/Cart";
-import FooterPres from "./components/Footer/FooterPres";
+// import FooterPres from "./components/Footer/FooterPres";
 //Requiere arreglos de estilo para renderizarse bien en el ItemDetail
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
