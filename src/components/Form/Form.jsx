8971687d596d3d1b0a93fc522035b7bee20dd2c5
Carlos Morales <carlosmora230190@gmail.com>
2023-03-24 15:14:45 -0300

    Añadiendo etiquetas a mi input

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+
+    const [name, setName] = useState("")
+
     return (
         <div>
             <form action="">
-                <input type="text" placeholder="ingrese su nombre" />
+                <input type="text" placeholder="ingrese su nombre" onChange={(event)=>console.log(event)} name="name"/>
                 <br />
                 <button>Enviar</button>
             </form>
