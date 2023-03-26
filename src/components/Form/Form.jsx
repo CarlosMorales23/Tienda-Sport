fcadbf3f38063c81b7428b87f02069254ca22676
Carlos Morales <carlosmora230190@gmail.com>
2023-03-26 13:53:06 -0300

    se añadio mesajaes de error para las validaciones

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,38 +2,19 @@ import { Button } from "@mui/material";
 import React, { useState } from "react";
 
 const Form = () => {
-    // const [name, setName] = useState("");
-    // const [email, setEmail] = useState("");
-
-    // const handleChangeNombre = (e) => {
-    //     setName(e.target.value);
-    // };
-
-    // const handleChangeEmail = (e) => {
-    //     setEmail(e.target.value);
-    // };
-
-    // console.log(name);
-    // console.log(email);
-
-    // const handleChange2 = (e) => {
-    //     setUserData({ ...userData, email: e.target.value });
-    // };
-
-    // const handleChange3 = (e) => {
-    //     setUserData({ ...userData, password: e.target.value });
-    // };
-
     const [userData, setUserData] = useState({
         name: "",
         email: "",
         password: "",
     });
 
+    const [error, setError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+
     const handleChange = (e) => {
         setUserData({ ...userData, [e.target.name]: e.target.value });
     };
-
+    // VALIDACIONES
     // name----------- minimo 15 caracteres
     // email ---------contenga un @
     // password ------ no espacios en blanco
@@ -43,20 +24,25 @@ const Form = () => {
 
         // validaciones
         if (userData.name.length < 5) {
-            console.log("Nombre no valido");
+            setError(true);
+            setErrorMessage("El nombre no puede ser menor a 5 caracteres");
             return;
         }
 
         const incluye = userData.email.includes("@");
+
         if (!incluye) {
-            console.log("email no valido");
+            setError(true);
+            setErrorMessage("El email dbe incluir un @");
             return;
         }
 
-        const str = userData.password.trim()
-        const passwordIsValid= userData.password === str
-        if(!passwordIsValid  || userData.password.length < 5 ){
-            console.log("la contraseña no debe tener espacion en blanco y minimo 5 carcteres")
+        const str = userData.password.trim();
+        const passwordIsValid = userData.password === str;
+
+        if (!passwordIsValid || userData.password.length < 5) {
+            setError(true);
+            setErrorMessage("El nombre no puede ser menor a 5 caracteres, y sin espacios");
         }
         // instrucciones
         console.log(userData);
@@ -93,8 +79,36 @@ const Form = () => {
                 <br />
                 <button type="submit">Enviar</button>
             </form>
+
+            {error && <h6>{errorMessage}</h6>}
+            
         </div>
     );
 };
 
 export default Form;
+
+// como refactorizar video 1:42:00, la idea seria un objeto de errores y pasar los objetos
+
+
+// const [name, setName] = useState("");
+// const [email, setEmail] = useState("");
+
+// const handleChangeNombre = (e) => {
+//     setName(e.target.value);
+// };
+
+// const handleChangeEmail = (e) => {
+//     setEmail(e.target.value);
+// };
+
+// console.log(name);
+// console.log(email);
+
+// const handleChange2 = (e) => {
+//     setUserData({ ...userData, email: e.target.value });
+// };
+
+// const handleChange3 = (e) => {
+//     setUserData({ ...userData, password: e.target.value });
+// };
