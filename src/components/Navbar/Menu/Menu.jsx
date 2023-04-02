0fa5644c2b4e7f70521180199a8d7b3399a0f505
Carlos Morales <carlosmora230190@gmail.com>
2023-04-01 21:01:16 -0300

    Se mapearon las categorias

--- src/components/Navbar/Menu/Menu.jsx
+++ src/components/Navbar/Menu/Menu.jsx
@@ -1,43 +1,41 @@
 import { Button, Stack } from "@mui/material";
 import React, { useEffect, useState } from "react";
 // import styles from "./Menu.module.css"
-import {Link} from "react-router-dom"
-import { collection, getDoc, getDocs } from "firebase/firestore";
+import { Link } from "react-router-dom";
+import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../../firebaseConfig";
 
-
-
 const Menu = () => {
-
-    const [categoryList, setcategoryList] = useState([])
+    const [categoryList, setcategoryList] = useState([]);
 
     useEffect(() => {
-        const itemsCollection = collection (db, "categories")
-        getDocs(itemsCollection)
-        .then((res)=> {
-            let arrayCategories = res.docs.map((category)=>{
+        const itemsCollection = collection(db, "categories");
+        getDocs(itemsCollection).then((res) => {
+            let arrayCategories = res.docs.map((category) => {
                 return {
                     ...category.data(),
-                    id: category.id
-                }
-            })
-            setcategoryList(arrayCategories)
-        })
+                    id: category.id,
+                };
+            });
+            setcategoryList(arrayCategories);
+        });
     }, []);
-    
-
-
 
     return (
         <ul>
-            {
-                categoryList.map((category)=>{
-                    return <Link>{category.title}</Link>
-                })
-            }
+            {categoryList.map((category) => {
+                return (
+                    <Link
+                        key={category.id}
+                        to={category.path}
+                        style={{ margin: "20px" }}
+                    >
+                        {category.title}
+                    </Link>
+                );
+            })}
         </ul>
     );
 };
 
 export default Menu;
-
