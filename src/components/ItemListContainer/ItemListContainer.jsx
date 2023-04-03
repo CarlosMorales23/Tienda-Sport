699420487aaf4fc659d5ce3f6141d7dc0922c146
Carlos Morales <carlosmora230190@gmail.com>
2023-04-02 22:47:02 -0300

    Codigo successfully

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,6 @@
-// import { Button, Stack } from "@mui/material";
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
-// import { products } from "../../productsMock";
 import ItemList from "../ItemList/ItemList";
 import CircularProgress from "@mui/material/CircularProgress";
 
