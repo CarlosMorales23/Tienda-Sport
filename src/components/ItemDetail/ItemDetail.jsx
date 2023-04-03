8d10c441eec856b6f6671c5dcec4a844fff99c37
Carlos Morales <carlosmora230190@gmail.com>
2023-04-02 22:38:11 -0300

    Se renderizo ItemDetail solo. Funcionando ok.

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import ItemCount from "../ItemCount/ItemCount";
 
-const ItemDetail = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
+const ItemDetail = ({productSelected, onAdd, quantity}) => {
+    return (
+        <div>
+            <h1>{productSelected.title}</h1>
+            <img src={productSelected.img} alt="" style={{ width: 200 }} />
+            <ItemCount
+                stock={productSelected.stock}
+                onAdd={onAdd}
+                initial={quantity}
+            />
+        </div>
+    );
+};
 
-export default ItemDetail
+export default ItemDetail;
