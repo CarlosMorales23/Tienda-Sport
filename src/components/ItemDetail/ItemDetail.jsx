a695d0d871052d63883058cebd2ad81e8062036c
Carlos Morales <carlosmora230190@gmail.com>
2023-04-04 18:52:08 -0300

    Se dejo descripción solo en detalle

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ItemCount from "../ItemCount/ItemCount";
+import { Typography } from "@mui/material";
 
 const ItemDetail = ({productSelected, onAdd, quantity}) => {
     return (
         <div>
             <h1>{productSelected.title}</h1>
             <img src={productSelected.img} alt="" style={{ width: 200 }} />
+            <Typography color="text.primary">
+                    {productSelected.description}
+                </Typography>
             <ItemCount
                 stock={productSelected.stock}
                 onAdd={onAdd}
